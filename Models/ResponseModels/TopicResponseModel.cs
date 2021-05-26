597d211828a6dcf9c7b1a89868d8e4702a98802c
AntonioSantanaCode <71064079+AntonioSantanaCode@users.noreply.github.com>
2021-05-26 14:09:13 +0200

    Made Massive changes
    Good programming session

---
+++ Models/ResponseModels/TopicResponseModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpSnackisApp.Models.ResponseModels
+{
+    public class TopicResponseModel
+    {
+        public string topicID { get; set; }
+        public Category category { get; set; }
+        public object threads { get; set; }
+        public string title { get; set; }
+        public DateTime createDate { get; set; }
+    }
+
+    public class Category
+    {
+        public string categoryID { get; set; }
+        public Topic[] topics { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public DateTime createDate { get; set; }
+    }
+
+    public class Topic
+    {
+        public string topicID { get; set; }
+        public object threads { get; set; }
+        public string title { get; set; }
+        public DateTime createDate { get; set; }
+    }
+
+
+}
--- Pages/TopicView.cshtml.cs
+++ Pages/TopicView.cshtml.cs
@@ -1,16 +1,46 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using CSharpSnackisApp.Models.ResponseModels;
+using CSharpSnackisApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace CSharpSnackisApp.Pages
 {
     public class TopicViewModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<IndexModel> _logger;
+        public List<TopicResponseModel> _topicResponseModels { get; set; }
+        private readonly SnackisAPI _client;
+        public string Message { get; set; }
+
+        [BindProperty (SupportsGet = true)]
+        public string categoryID { get; set; }
+
+        public TopicViewModel(ILogger<IndexModel> logger, SnackisAPI client)
         {
+            _logger = logger;
+            _client = client;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            HttpResponseMessage response = await _client.GetAsync($"/Post/ReadTopicsInCategory/{categoryID}");
+            var request = response.Content.ReadAsStringAsync().Result;
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                _topicResponseModels = JsonConvert.DeserializeObject<List<TopicResponseModel>>(request);
+
+                return Page();
+            }
+            else
+                return RedirectToPage("/Error");
         }
     }
 }
