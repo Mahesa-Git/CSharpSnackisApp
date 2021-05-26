498b32642c6f0a7fc2195607f9ad056063703148
Mahesa-Git <71064153+Mahesa-Git@users.noreply.github.com>
2021-05-26 10:42:34 +0200

    DI with HttpClient service

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,17 +4,19 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace CSharpSnackisApp.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly HttpClient _client;
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, HttpClient client)
         {
             _logger = logger;
+            _client = client;
         }
 
         public void OnGet()
---
+++ Services/SnackisAPI.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpSnackisApp.Services
+{
+    public class SnackisAPI
+    {
+    }
+}
