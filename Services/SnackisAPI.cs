cd02e3347e9c5f458d67505707247d4a70f3a6a9
Mahesa-Git <71064153+Mahesa-Git@users.noreply.github.com>
2021-05-26 10:43:08 +0200

    some moew woops

--- Services/SnackisAPI.cs
+++ Services/SnackisAPI.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace CSharpSnackisApp.Services
 {
     public class SnackisAPI
     {
+        public HttpClient InitClient { get; init; }
+        public SnackisAPI()
+        {
+            InitClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:44302")
+            };
+        }
+
     }
 }
