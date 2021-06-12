0de61c4069511a232aaf7ce39c509ae469ad671d
Mahesa-Git <71064153+Mahesa-Git@users.noreply.github.com>
2021-06-12 22:24:08 +0200

    Added the bigining oof an APIgateway

--- Gateway/APIGateway.cs
+++ Gateway/APIGateway.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using CSharpSnackisApp.Services;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CSharpSnackisApp.Gateway
 {
-    public class APIGateway
+    public class APIGateway : Models.IAPIGateway
     {
+        private readonly IConfiguration _configuration;
+        private readonly SnackisAPI _client;
+        public APIGateway(SnackisAPI client, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _client = client;
+        }
     }
 }
